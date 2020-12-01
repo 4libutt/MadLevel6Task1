44944358a2c3b34c0d57c11513b2443ebfc5e85d
4libutt <ali@vergelijkgroep.nl>
2020-12-01 15:27:56 +0100

    connected viewmodel to ColorFragment class

--- app/src/main/java/com/example/madlevel6task1/ColorFragment.kt
+++ app/src/main/java/com/example/madlevel6task1/ColorFragment.kt
@@ -5,9 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.madlevel6task1.model.ColorItem
+import com.example.madlevel6task1.viewmodel.ColorViewModel
 import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.fragment_color.*
 
@@ -15,6 +18,8 @@ import kotlinx.android.synthetic.main.fragment_color.*
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 class ColorFragment : Fragment() {
+
+    private val viewModel: ColorViewModel by viewModels()
 
     private val colors = arrayListOf<ColorItem>()
     private lateinit var colorAdapter: ColorAdapter
@@ -39,5 +44,13 @@ class ColorFragment : Fragment() {
 
     private fun onColorClick(colorItem: ColorItem) {
         Snackbar.make(rvColors, "This color is: ${colorItem.name}", Snackbar.LENGTH_LONG).show()
+    }
+
+    private fun observeColors() {
+        viewModel.colorItems.observe(viewLifecycleOwner, Observer {
+            colors.clear()
+            colors.addAll(it)
+            colorAdapter.notifyDataSetChanged()
+        })
     }
 }
